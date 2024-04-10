ec1b09be656d8250c2f6cf551271daa65420e336
Анастасия <vika.tay.doo@gmail.com>
2024-04-10 22:46:35 +0300

    html change2

---
+++ data/jobs.py
@@ -0,0 +1,18 @@
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
+from sqlalchemy_serializer import SerializerMixin
+
+from .db_session import SqlAlchemyBase
+
+
+class Jobs(SqlAlchemyBase, SerializerMixin):
+    __tablename__ = 'jobs'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.Integer,
+                             nullable=True)
+    price = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    desc = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    img = sqlalchemy.Column(sqlalchemy.String, nullable=True)
