549f29d4edcc9de1dcbbdb459745fce6db36de52
Анастасия <vika.tay.doo@gmail.com>
2024-04-21 16:36:43 +0300

    goods api

---
+++ data/api/jobs_api.py
@@ -0,0 +1,38 @@
+import flask
+from flask import jsonify, make_response, request
+
+from data import db_session
+from data.jobs import Jobs
+
+blueprint = flask.Blueprint(
+    'jobs_api',
+    __name__,
+    template_folder='templates'
+)
+
+
+@blueprint.route('/api/jobs')
+def get_jobs():
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).all()
+    return jsonify(
+        {
+            'jobs':
+                [item.to_dict(only=('id', 'name', 'price', 'desc', 'img', 'type'))
+                 for item in jobs]
+        }
+    )
+
+
+@blueprint.route('/api/jobs/<int:jobs_id>', methods=['GET'])
+def get_one_jobs(jobs_id):
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).get(jobs_id)
+    if not jobs:
+        return make_response(jsonify({'error': 'Not found'}), 404)
+    return jsonify(
+        {
+            'jobs': jobs.to_dict(only=(
+                'id', 'name', 'price', 'desc', 'img', 'type'))
+        }
+    )
